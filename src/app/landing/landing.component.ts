afa25ce72bce04090f704aaec661bf5cb694c2c7
KBENBENECK <kbenbeneck@gmail.com>
2021-07-10 15:51:50 -0400

    Add scroll to about from landing button.

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -6,7 +6,7 @@ import { Component } from "@angular/core";
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
-  onClickToView() {
-
+  scrollToAbout() {
+    document.getElementById("about").scrollIntoView({behavior: "smooth"})
   }
 }
