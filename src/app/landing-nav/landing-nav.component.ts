e58389c5045d9cd580f87a92293bd5ecea050fb5
KBENBENECK <kbenbeneck@gmail.com>
2021-07-19 17:47:25 -0400

    Add new Landing Nav Comp with sidenav drawer.

---
+++ src/app/landing-nav/landing-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'landing-nav',
+  templateUrl: './landing-nav.component.html',
+  styleUrls: ['./landing-nav.component.css']
+})
+
+export class LandingNavComponent {
+
+}
